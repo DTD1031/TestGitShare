f5a758a81dde6db793d2aab4714563204a4ef0df
lecheng.chen <clc_cfzxyq@163.com>
2020-05-31 17:20:33 +0800

    try add code

---
+++ goodRepoTestObject.h
@@ -0,0 +1,16 @@
+//
+//  goodRepoTestObject.h
+//  TestGitShare
+//
+//  Created by 陈乐诚 on 2020/5/31.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface goodRepoTestObject : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ goodRepoTestObject.m
@@ -0,0 +1,12 @@
+//
+//  goodRepoTestObject.m
+//  TestGitShare
+//
+//  Created by 陈乐诚 on 2020/5/31.
+//
+
+#import "goodRepoTestObject.h"
+
+@implementation goodRepoTestObject
+
+@end
